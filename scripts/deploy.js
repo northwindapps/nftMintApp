9d57ee47e9b10da5afaf3fd3a584486085734a6f
Yujin Yano <northwindapps@gmail.com>
2023-01-09 21:35:42 +0900

    solidity env  deployable

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,19 +7,21 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
 
   const RoboPunksNFT = await hre.ethers.getContractFactory("RoboPunksNFT");
-  const roboPunksNFT = await RoboPunksNFT.deploy(unlockTime, { value: lockedAmount });
+  // const roboPunksNFT = await RoboPunksNFT.deploy(unlockTime, { value: lockedAmount });
+  const roboPunksNFT = await RoboPunksNFT.deploy();
+
 
   await roboPunksNFT.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${roboPunksNFT.address}`
+    `RoboPunksNFT deployed to ${roboPunksNFT.address}`
   );
 }
 
